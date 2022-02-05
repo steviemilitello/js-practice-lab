228ff2090b6f122f276baefb29128c7d73f1785f
Timm Schoenborn <timmschoenborn@gmail.com>
2022-02-05 16:56:41 -0500

    updates readmes and instructions

---
+++ bin/99bottles.js
@@ -0,0 +1,2 @@
+// Open the file in this js-practice repo in the bin folder named 99bottles.js
+// Running the program should print the lyrics of the song, be efficient and use loops/if statements
---
+++ bin/rps.js
@@ -0,0 +1,7 @@
+//   Open the file in this js-practice repo in the bin folder named rps.js
+//     - Write a script that looks at a variable representing either rock, paper,
+//       or scissors, and picks a random one to pit it against.
+//     - Log "Computer picked: <rock/paper/scissors>. You <win/lose>".
+//     - _**Hint:** Try using Numbers to relate to user choices like using 0 to refer
+//     to rock, 1 for paper, and 2 for scissors. Javascript includes a `random`
+//     method within the `Math` built-in object to help you generate these numbers._
